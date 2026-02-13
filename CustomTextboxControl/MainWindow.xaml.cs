70cd11ef4e476800901a09ef84159a0f507bd0da
Sky_Time87 <blackshadow1609@yandex.ru>
2026-02-13 08:19:26 +0300

    Placeholder DONE

---
+++ CustomTextboxControl/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomTextboxControl
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			this.Icon = new BitmapImage(new Uri("D:\\Users\\wwwbl\\source\\repos\\WPF\\ico\\text_box.ico")); /*Добавление значка в главное окно WPF-приложения*/
+		}
+	}
+}
